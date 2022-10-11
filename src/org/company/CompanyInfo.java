05b8e0d705a1b84bb501806cfb29ca959c209c80
Vivekachandran <vivekachandran12@gmail.com>
2022-10-11 11:28:47 -0700

    added file

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,18 @@
+package org.company;
+
+public class CompanyInfo  {
+	private void companyName() {
+		// TODO Auto-generated method stub
+System.out.println("hitech");
+	}
+	private void companyName(int id,char g) {
+		// TODO Auto-generated method stub
+System.out.println("company id:"+id);
+System.out.println("g:"+ g);
+	}
+	public static void main(String[] args) {
+		CompanyInfo c=new CompanyInfo();
+		c.companyName();
+		c.companyName(316531,'f');
+	}
+}
